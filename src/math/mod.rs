1ebd310fdf05439d85486be963099eb62ca24aed
Marshall Bowers <elliott.codes@gmail.com>
2018-07-14 12:10:59 -0400

    cargo fmt

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -36,11 +36,11 @@ mod trunc;
 mod truncf;
 
 pub use self::{
-     atanf::atanf, ceilf::ceilf, cosf::cosf, expf::expf, fabs::fabs, fabsf::fabsf, floor::floor, floorf::floorf,
-    fmodf::fmodf, hypot::hypot, hypotf::hypotf, log::log, log10::log10, log10f::log10f,
-    log1p::log1p, log1pf::log1pf, log2::log2, log2f::log2f, logf::logf, powf::powf, round::round,
-    roundf::roundf, scalbn::scalbn, scalbnf::scalbnf, sqrt::sqrt, sqrtf::sqrtf, trunc::trunc,
-    truncf::truncf,
+    atanf::atanf, ceilf::ceilf, cosf::cosf, expf::expf, fabs::fabs, fabsf::fabsf, floor::floor,
+    floorf::floorf, fmodf::fmodf, hypot::hypot, hypotf::hypotf, log::log, log10::log10,
+    log10f::log10f, log1p::log1p, log1pf::log1pf, log2::log2, log2f::log2f, logf::logf, powf::powf,
+    round::round, roundf::roundf, scalbn::scalbn, scalbnf::scalbnf, sqrt::sqrt, sqrtf::sqrtf,
+    trunc::trunc, truncf::truncf,
 };
 
 mod k_cosf;
