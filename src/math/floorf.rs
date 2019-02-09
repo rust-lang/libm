46c84888367f8b0b19cbb6cae5a142b0d4dce22f
Shinichi Kogai <kogai0121@gmail.com>
2019-02-09 23:19:09 +0900

    Wrap some arithmetic

--- src/math/floorf.rs
+++ src/math/floorf.rs
@@ -11,7 +11,7 @@ pub fn floorf(x: f32) -> f32 {
         }
     }
     let mut ui = x.to_bits();
-    let e = (((ui >> 23) & 0xff) - 0x7f) as i32;
+    let e = (((ui >> 23) & 0xff).wrapping_sub(0x7f)) as i32;
 
     if e >= 23 {
         return x;
