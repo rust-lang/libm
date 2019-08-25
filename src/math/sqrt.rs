3894b7972d9db8170d3fe5ea780c6df135cc7f13
Lokathor <zefria@gmail.com>
2019-08-25 10:01:46 -0600

    Update sqrt.rs
    Change per the updated info in https://github.com/rust-lang/rust/pull/63455#issuecomment-524638624

--- src/math/sqrt.rs
+++ src/math/sqrt.rs
@@ -20,8 +20,8 @@ pub fn sqrt(x: f64) -> f64 {
     #[cfg(all(
         not(feature = "stable"),
         any(
-            target_arch = "x86",
-            target_arch = "x86_64",
+            all(target_arch = "x86", not(target_feature="soft_float")),
+            all(target_arch = "x86_64", not(target_feature="soft_float")),
             target_arch = "aarch64",
             target_arch = "wasm32",
             target_arch = "powerpc64"
