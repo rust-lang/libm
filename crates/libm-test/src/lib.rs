905fba3b900b5ee80e7b97db435bd6c8826a0a17
Trevor Gross <tmgross@umich.edu>
2024-10-16 22:55:05 -0500

    Add traits for testing
    These traits give us a more generic way to interface with tuples used
    for (1) test input, (2) function arguments, and (3) test input.

--- crates/libm-test/src/lib.rs
+++ crates/libm-test/src/lib.rs
@@ -1,6 +1,8 @@
 mod num_traits;
+mod test_traits;
 
 pub use num_traits::{Float, Hex, Int};
+pub use test_traits::{CheckBasis, CheckCtx, CheckOutput, GenerateInput, TupleCall};
 
 // List of all files present in libm's source
 include!(concat!(env!("OUT_DIR"), "/all_files.rs"));
