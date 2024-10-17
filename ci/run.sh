9dd315644a578e16b3237dbaf8a65947997b47c8
Trevor Gross <tmgross@umich.edu>
2024-10-16 22:59:59 -0500

    Enable more targets on CI
    This brings the targets tested here in line with those tested in
    `compiler-builtins`.

--- ci/run.sh
+++ ci/run.sh
@@ -2,21 +2,49 @@
 
 set -eux
 
-target="$1"
-
-cmd="cargo test --all --target $target"
-
+export RUST_BACKTRACE="${RUST_BACKTRACE:-full}"
 # Needed for no-panic to correct detect a lack of panics
 export RUSTFLAGS="$RUSTFLAGS -Ccodegen-units=1"
 
-# stable by default
-$cmd
-$cmd --release
+target="${1:-}"
 
-# unstable with a feature
-$cmd --features 'unstable'
-$cmd --release --features 'unstable'
+if [ -z "$target" ]; then
+    host_target=$(rustc -vV | awk '/^host/ { print $2 }')
+    echo "Defaulted to host target $host_target"
+    target="$host_target"
+fi
 
-# also run the reference tests
-$cmd --features 'unstable libm-test/musl-bitwise-tests'
-$cmd --release --features 'unstable libm-test/musl-bitwise-tests'
+
+# We nceed to specifically skip tests for this crate on systems that can't
+# build musl since otherwise `--all` will activate it.
+case "$target" in
+    *msvc*) exclude_flag="--exclude musl-math-sys" ;;
+    *wasm*) exclude_flag="--exclude musl-math-sys" ;;
+    *thumb*) exclude_flag="--exclude musl-math-sys" ;;
+    *) exclude_flag="" ;;
+esac
+
+if [ "${BUILD_ONLY:-}" = "1" ]; then
+    cmd="cargo build --target $target --package libm"
+    $cmd
+    $cmd --features 'unstable'
+
+    echo "no tests to run for no_std"
+else
+    cmd="cargo test --all --target $target $exclude_flag"
+
+
+    # stable by default
+    $cmd
+    $cmd --release
+
+    # unstable with a feature
+    $cmd --features 'unstable'
+    $cmd --release --features 'unstable'
+
+    if [ "$(uname -a)" = "Linux" ]; then
+        # also run the reference tests when we can. requires a Linux host.
+        $cmd --features 'unstable libm-test/musl-bitwise-tests'
+        $cmd --release --features 'unstable libm-test/musl-bitwise-tests'
+    fi
+fi
