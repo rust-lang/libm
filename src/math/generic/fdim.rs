0b9b61f6529d2ee101130c5557f6c6c41f6aabeb
quaternic <57393910+quaternic@users.noreply.github.com>
2025-01-14 03:34:59 +0200

    fmt

--- src/math/generic/fdim.rs
+++ src/math/generic/fdim.rs
@@ -1,9 +1,5 @@
 use super::super::Float;
 
 pub fn fdim<F: Float>(x: F, y: F) -> F {
-    if x <= y {
-        F::ZERO
-    } else {
-        x - y
-    }
+    if x <= y { F::ZERO } else { x - y }
 }
